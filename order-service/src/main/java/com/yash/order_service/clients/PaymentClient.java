98d53114163e29155a9e1a1fa3f1901c8fec0303
98001yash <yashchauhan.gaya@gmail.com>
2026-04-09 16:35:02 +0530

    added the circuit breaker

--- order-service/src/main/java/com/yash/order_service/clients/PaymentClient.java
+++ order-service/src/main/java/com/yash/order_service/clients/PaymentClient.java
@@ -5,7 +5,7 @@ package com.yash.order_service.clients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "payment-service", url = "http://payment-service:9020")
+@FeignClient(name = "payment-service", url = "${payment-service.url}")
 public interface PaymentClient {
 
     @GetMapping("/pay")
